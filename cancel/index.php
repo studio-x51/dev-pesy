d029b520aebdece1794bb30da8546b17192d9dac
Petr Syrný <kontakt@petrsyrny.cz>
2016-03-19 15:32:02 +0100

    Commited file from IDE

---
+++ cancel/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="cs-CZ">
+<head>
+<meta charset="utf-8">
+
+<title>CHCI ZRUŠIT ČLENSTVÍ SLUŽBY SOCIALSPRINTERS</title>
+
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, maximum-scale=1">
+<meta name="robots" content="noindex, nofollow">
+
+<meta property="og:title" content="">
+<meta property="og:description" content="">
+<meta property="og:image" content="">
+<meta property="og:url" content="">
+
+<link rel="shortcut icon" href="favicon.ico">
+
+<link rel="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,700&amp;subset=latin,latin-ext">
+<link rel="stylesheet" href="css/style.css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>!window.jQuery && document.write('<script src="js/jquery.min.js"><\/script>')</script>
+<script src="js/main.js"></script>
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+<!-- GA - měřící kód -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-55151547-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '631173753652934');
+fbq('track', "PageView");
+fbq('track', 'Lead');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=631173753652934&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+<div id="main">
+
+<header id="header">
+
+<a href="http://www.socialsprinters.cz/" title="SocialSprinters [Úvodní stránka]" id="logo">SocialSprinters<span></span></a>
+
+</header>
+
+<div class="content clearfix">
+
+	<h1>CHCI ZRUŠIT ČLENSTVÍ SLUŽBY SOCIALSPRINTERS</h1>
+
+	<div class="col fl">
+	<img src="img/cancel.png" alt="" width="511" height="360">
+	<h2>Škoda, že nás opouštíte. :(</h2>
+	<p>Zrušením členství ztratíte přístup ke všem aplikacím a všechny výhody, které jste spolu s členstvím získali.</p>
+	<ul>
+		<li>Vyplňte následující údaje ve formuláři.</li>
+		<li>Prosím o vyplnění přesných údajů. Nesprávné vyplnění údajů akorát zpomalí váš požadavek.</li>
+	</ul>
+	</div>
+
+	<div class="col fr">
+	<?php include 'inc/form.php' ?>
+	</div>
+	
+</div><!--/content-->
+
+<footer id="footer">
+	<small>
+		&copy; <?php echo date("Y") ?> <strong>SocialSprinters</strong>
+	</small>
+</footer>
+
+</div><!--/main-->
+
+</body>
+</html>

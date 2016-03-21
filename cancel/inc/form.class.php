a8b64dc5ace4ea2af7e435ebb20213da7aaf45a6
Petr Syrný <kontakt@petrsyrny.cz>
2016-03-21 17:02:02 +0100

    Fix of sendCancelForm, doesn't work on server with static function toSafeData. So input values are only trimmed.

--- cancel/inc/form.class.php
+++ cancel/inc/form.class.php
@@ -2,53 +2,46 @@
 /**
  * Description of form class
  *
- * @author pesy
+ * @author pesy petr.syrny@x51.cz
  */
 class Form {
 
-  /*@var array of errors*/
-  public $error = array(); 
   /*@var value of error message*/
   public $errorMessage = null;
   /*@var value of success message*/
   public $successMessage = null;
   
-  /** Funkce oescapuje string z inputu, pro vlozeni do db
+  /** Escape string, from input, insert to database etc.
    * @param string $string value to escape
    * @return string escaped string
    * @author pesy petr.syrny@x51.cz
    */
-  static function toSafeData($string) {
+  public function toSafeData($string) {
     if (get_magic_quotes_gpc()) {
       $string = stripslashes($string);
     }
     $string = mysql_real_escape_string(htmlspecialchars($string));
-    return $string;
+    return trim($string);
   }    
   
 	/**
-	* Fuknce se stara o drzeni hodnoty v danem elementu formulare, odeslani POSTu
-   * @param string $form_element - hodnota (name) elementu $_POST['']
+	* Keep value of form input, mainly for POST
+   * @param string $form_element - value (name) of element $_POST['']
    * @return string value of input, only for type = 'text' || area
 	 * @author pesy petr.syrny@x51.cz
 	*/
-	public function keepValueRet($form_element) {
-		if (isset($form_element)) return (string)$form_element;
-	}	  
-  
-  /** Funkce vraci pole chyb
-   */
-  public function getError() {
-    if (isset($this->error)&&(is_array($this->error))&&(count($this->error)>0)) {
-      return $this->error;
-    }
-    return;
+  public function keepValueRet($form_element) {
+    if (isset($form_element)) { return (string)$form_element; }
   }
   
-	public function sendCancelForm($vals) {
-    $firstname = self::toSafeData($vals['cancel_firstname']);
-    $lastname = self::toSafeData($vals['cancel_lastname']);
-	  $email = self::toSafeData($vals['cancel_email']);
+  /**
+   * Submit action for cancel form
+   * - check input values, set error message, header if success
+   */
+  public function sendCancelForm($vals) {
+    $firstname = trim($vals['cancel_firstname']);
+    $lastname = trim($vals['cancel_lastname']);
+    $email = trim($vals['cancel_email']);
     $submit = $vals['cancel_send'];
     
     if (isset($submit)) {
@@ -67,7 +60,7 @@ class Form {
         return false;
       }
     }
-		if ($_SESSION['cancel_send_success']) {
+    if ($_SESSION['cancel_send_success']) {
       $this->setSuccessMessage("<strong>Vaše žádost byla zaznamenána.</strong>");
       $this->errorMessage = null;
       $_SESSION['cancel_send_success'] = false;
@@ -108,7 +101,7 @@ class Form {
    */
   public function getSuccessMessage() {
     if (isset($this->successMessage)&&(!empty($this->successMessage))) {
-      return '<div class="alert alert-success text-center" role="alert">'.$this->successMessage.'</div>';
+      return '<div class="alert alert-success tac" role="alert">'.$this->successMessage.'</div>';
     }
   }  
   
